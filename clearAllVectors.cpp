966483cbb9d1e1ab82cedf2c6e813410b7804874
kshahamat <75522104+kshahamat@users.noreply.github.com>
2022-08-23 12:02:45 -0400

    Create clearAllVectors.cpp

---
+++ clearAllVectors.cpp
@@ -0,0 +1,20 @@
+//clear all data
+bool clearAllVectors(vector<string>& gender, vector<int>& age, vector<int>& totalCholesterol, vector<double>& HDL,
+	vector<double>& SBP, vector<string>& HTN, vector<string>& SmokingStatus)
+{
+	gender.clear();
+	age.clear();
+	totalCholesterol.clear();
+	HDL.clear();
+	SBP.clear();
+	HTN.clear();
+	SmokingStatus.clear();
+
+	if (!gender.empty() || !age.empty() || !totalCholesterol.empty() || !HDL.empty() || !SBP.empty() || !HTN.empty() || !SmokingStatus.empty())
+		return false;
+	else
+	{
+		return true;
+	}
+
+};
